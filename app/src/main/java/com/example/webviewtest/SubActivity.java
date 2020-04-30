176cfeb587620f5b1e368c19abfcdfca53586f20
limjongwoo <whddn528@naver.com>
2020-04-30 11:08:01 +0900

    camera

--- app/src/main/java/com/example/webviewtest/SubActivity.java
+++ app/src/main/java/com/example/webviewtest/SubActivity.java
@@ -3,38 +3,42 @@ package com.example.webviewtest;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
-import android.Manifest;
-import android.annotation.TargetApi;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.PermissionRequest;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 
 public class SubActivity extends AppCompatActivity {
 
     private static final int FILE_REQUEST_CODE = 9999;
-    private ValueCallback mFilePathCallback;
+    private static final int FILECHOOSER_RESULTCODE = 1111;
+    /*private ValueCallback mFilePathCallback;*/
     private WebView webView;
     private WebSettings webSettings;
-//    private String url = "https://foodimage.netlify.com";
-    private String url = "file:///android_asset/www/index.html";
-
+    private String url = "https://foodimage.netlify.com";
+    //private String url = "file:///android_asset/www/index.html";
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private Uri mCapturedImageURI = null;
+    private String mCameraPhotoPath;
+    private static final String TAG = SubActivity.class.getSimpleName();
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,41 +50,150 @@ public class SubActivity extends AppCompatActivity {
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(true);
+        webSettings.setAllowFileAccess(true);
         webView.loadUrl(url);
         webView.setWebViewClient(new webViewClientclass());
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
         webView.setWebChromeClient(new WebChromeClient() {
-
+            // 카메라 허용 권한
             @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback filePathCallback, FileChooserParams fileChooserParams) {
+            public void onPermissionRequest(final PermissionRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback,
+                                             FileChooserParams fileChooserParams) {
+
+                if (mFilePathCallback != null) {
+                    mFilePathCallback.onReceiveValue(null);
+                }
                 mFilePathCallback = filePathCallback;
 
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
 
-                startActivityForResult(intent, FILE_REQUEST_CODE);
+                    // create the file where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                    } catch (IOException ex) {
+
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
+
+                    // continue only if the file was successfully created
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, getString(R.string.image_chooser));
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+
                 return true;
+
             }
         });
     }
 
+    // creating image files (Lollipop only)
+    private File createImageFile() throws IOException {
+
+        File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "DirectoryNameHere");
+
+        if (!imageStorageDir.exists()) {
+            imageStorageDir.mkdirs();
+        }
+
+        // create an image file name
+        imageStorageDir  = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+        return imageStorageDir;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
+        // code for all versions except of Lollipop
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
 
-        if(requestCode == FILE_REQUEST_CODE && resultCode == RESULT_OK){
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                mFilePathCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
-            }else{
-                mFilePathCallback.onReceiveValue(new Uri[]{data.getData()});
+            if(requestCode==FILECHOOSER_RESULTCODE) {
+                if (null == this.mUploadMessage) {
+                    return;
+                }
+
+                Uri result=null;
+
+                try{
+                    if (resultCode != RESULT_OK) {
+                        result = null;
+                    } else {
+                        // retrieve from the private variable if the intent is null
+                        result = data == null ? mCapturedImageURI : data.getData();
+                    }
+                }
+                catch(Exception e) {
+                    Toast.makeText(getApplicationContext(), "activity :"+e, Toast.LENGTH_LONG).show();
+                }
+
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
             }
+
+        } // end of code for all versions except of Lollipop
+
+        // start of code for Lollipop only
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            if (requestCode != FILECHOOSER_RESULTCODE || mFilePathCallback == null) {
+                super.onActivityResult(requestCode, resultCode, data);
+                return;
+            }
+
+            Uri[] results = null;
+
+            // check that the response is a good one
+            if (resultCode == Activity.RESULT_OK) {
+                if (data == null || data.getData() == null) {
+                    // if there is not data, then we may have taken a photo
+                    if (mCameraPhotoPath != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                    }
+                } else {
+                    String dataString = data.getDataString();
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+                    }
+                }
+            }
+
+            mFilePathCallback.onReceiveValue(results);
             mFilePathCallback = null;
-        }else{
-            mFilePathCallback.onReceiveValue(null);
-        }
+
+        } // end of code for Lollipop only
+
     }
 
 
@@ -96,7 +209,6 @@ public class SubActivity extends AppCompatActivity {
 
 
 
-
     private class webViewClientclass extends WebViewClient {
 
         @Override
@@ -105,7 +217,6 @@ public class SubActivity extends AppCompatActivity {
             return true;
         }
     }
-
 
 
 }
